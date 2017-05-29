bf7e2344b35a1cb51bd1a84c10f2f77ba4c04312
qpz <Think@DESKTOP-D0VQSHJ>
2017-05-29 11:11:27 +0800

    获取运输方式

--- src/main/java/com/aohua/controller/Add_OrderController.java
+++ src/main/java/com/aohua/controller/Add_OrderController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.aohua.service.DepartMentService;
+import com.aohua.service.TransportModelService;
 
 /**
  * qin is prefect
@@ -21,10 +22,22 @@ import com.aohua.service.DepartMentService;
 public class Add_OrderController {
 	@Autowired
 	private DepartMentService departMentService;
+	@Autowired
+	private TransportModelService transportModelService;
+	//获取所属部门列表
 	@RequestMapping("getdepartment")
-	public void getUserid(HttpServletRequest req,HttpServletResponse res ) throws IOException{
+	public void getdepartment(HttpServletRequest req,HttpServletResponse res ) throws IOException{
 		JSONArray jsonObject=JSONArray.fromObject(departMentService.getDepartMent());
 		res.setContentType("text/html;charset=utf-8");
 		res.getWriter().println(jsonObject.toString());
+		System.out.println(jsonObject.toString());
+	}
+	//获取运输方式列表
+	@RequestMapping("gettransportmodel")
+	public void gettransportmodel(HttpServletRequest req,HttpServletResponse res ) throws IOException{
+		JSONArray jsonObject=JSONArray.fromObject(transportModelService.getTransportMode());
+		res.setContentType("text/html;charset=utf-8");
+		res.getWriter().println(jsonObject.toString());
+		System.out.println(jsonObject.toString());
 	}
 }
--- src/main/java/com/aohua/dao/DepartMentDao.java
+++ src/main/java/com/aohua/dao/DepartMentDao.java
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class DepartMentDao {
 	@Autowired
-	private JdbcTemplate jbTemplate;
+	private JdbcTemplate jdbcTemplate;
 	
 	public List<Map<String,Object>> getDepartMent(){
 		String sql="select DeptID,DeptName from department";
-		List<Map<String,Object>> list=jbTemplate.queryForList(sql);
+		List<Map<String,Object>> list=jdbcTemplate.queryForList(sql);
 //		Map<Integer,String> returnMap=new HashMap<Integer, String>();
 //		for(int i=0;i<list.size();i++){
 //			returnMap.put((Integer)list.get(i).get("DeptID"), (String)list.get(i).get("DeptName"));
